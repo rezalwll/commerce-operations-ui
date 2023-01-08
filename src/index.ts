748610520e79d0ee0fd0e8f834a2180cea2361cf
Mohamadreza <243572919+rezalwll@users.noreply.github.com>
2023-01-08 12:47:00 +0330

    feat(settings): derive focused capacity

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { calculateInitialTotal } from './features/initial-total';
 export { MetricCard } from './components/MetricCard';
 export { resolveSessionsFocusedCapacity } from './features/sessions-resolve-focused-capacity';
+export { deriveSettingsFocusedCapacity } from './features/settings-derive-focused-capacity';
